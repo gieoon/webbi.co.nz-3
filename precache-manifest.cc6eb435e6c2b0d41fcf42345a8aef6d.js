5b550c774ea736848b5e4e0cded643bb4d470827
gieoon <jun.a.kagaya@gmail.com>
2021-12-30 12:14:03 +1300

    Updates

---
+++ precache-manifest.cc6eb435e6c2b0d41fcf42345a8aef6d.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85b4b9341e2120c3310915b432d5f26a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7438b6384cbd3172867",
+    "url": "/static/css/main.1a3bf495.chunk.css"
+  },
+  {
+    "revision": "abdd750a2bb12af82900",
+    "url": "/static/js/2.318bc414.chunk.js"
+  },
+  {
+    "revision": "668036a1c673158136615d0f8dd36026",
+    "url": "/static/js/2.318bc414.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a7438b6384cbd3172867",
+    "url": "/static/js/main.15dc1134.chunk.js"
+  },
+  {
+    "revision": "a24f81ee3e99020583eb",
+    "url": "/static/js/runtime-main.e1728e9e.js"
+  },
+  {
+    "revision": "3715df325276d4b10fbb2282e05dc089",
+    "url": "/static/media/bush_and_beyond.3715df32.png"
+  },
+  {
+    "revision": "21049a09517525b472da225c80cee356",
+    "url": "/static/media/bush_and_beyond_b4.21049a09.png"
+  },
+  {
+    "revision": "da17dfaed7683d20b583f07bd2c50521",
+    "url": "/static/media/covid19live.da17dfae.png"
+  },
+  {
+    "revision": "314c0ef07b04d423b2dc576d29950c26",
+    "url": "/static/media/different.314c0ef0.svg"
+  },
+  {
+    "revision": "63b06a10bb403494562c1348080daa17",
+    "url": "/static/media/historinz.63b06a10.png"
+  },
+  {
+    "revision": "f2681b2e300ef8bc8a7ab1afc55478f1",
+    "url": "/static/media/historinz_b4.f2681b2e.png"
+  },
+  {
+    "revision": "5d45e87339de55617925b657f6b2a21e",
+    "url": "/static/media/imhired.5d45e873.png"
+  },
+  {
+    "revision": "eba8252b1b4afe691118aa2b2740fea9",
+    "url": "/static/media/j-signal2.eba8252b.png"
+  },
+  {
+    "revision": "7de70f97115a33ad3f8f466a9b991b20",
+    "url": "/static/media/justsayheyy.7de70f97.png"
+  },
+  {
+    "revision": "aae78d5ada767a21ed1281c52cef37ad",
+    "url": "/static/media/nzcommute.aae78d5a.png"
+  },
+  {
+    "revision": "7cf357194f453387b3fccdb6c591de25",
+    "url": "/static/media/nzcovid19live.7cf35719.png"
+  },
+  {
+    "revision": "82db3258d52ac5e1b835bde0b279ff3b",
+    "url": "/static/media/ohmyperson.82db3258.png"
+  },
+  {
+    "revision": "cd289b31a252eeba6b3ef31eee9d0277",
+    "url": "/static/media/orange.cd289b31.gif"
+  },
+  {
+    "revision": "e75ab79e3a04952d5c6aba6103324073",
+    "url": "/static/media/piggybank.e75ab79e.svg"
+  },
+  {
+    "revision": "a124f6e56783d93570b5e1162a38e807",
+    "url": "/static/media/processhub.a124f6e5.png"
+  },
+  {
+    "revision": "ec52024860a0587920ca97ae3440e2ee",
+    "url": "/static/media/reeftonhideaway.ec520248.png"
+  },
+  {
+    "revision": "c7b29c5cdcab791e17a4a3d1599f72cc",
+    "url": "/static/media/resolve.c7b29c5c.png"
+  },
+  {
+    "revision": "f1bc7f03410a91bc7744caab9e911d0a",
+    "url": "/static/media/rocket.f1bc7f03.svg"
+  },
+  {
+    "revision": "bdfd77f43c1cc4cdee5f29993f68329d",
+    "url": "/static/media/shield.bdfd77f4.svg"
+  },
+  {
+    "revision": "87deaf0c001ac4d5b2d0d37977f99e7e",
+    "url": "/static/media/time.87deaf0c.svg"
+  },
+  {
+    "revision": "4115708373b38c7eee0532550a4cbb9c",
+    "url": "/static/media/title2_background.41157083.jpg"
+  },
+  {
+    "revision": "37f4ba55e9209199512fc5e145a5e5e7",
+    "url": "/static/media/unicorn.37f4ba55.svg"
+  },
+  {
+    "revision": "accc7fbf7a61e6fa55be718b52a673f6",
+    "url": "/static/media/whowrotethistext.accc7fbf.png"
+  },
+  {
+    "revision": "a4d75d14c6f894921a030baa171e4e03",
+    "url": "/static/media/witch.a4d75d14.svg"
+  }
+]);
